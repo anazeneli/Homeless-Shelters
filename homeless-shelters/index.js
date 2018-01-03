8fc8f709fb4dbb104a7bb93fabdeaa48df7ba873
Sarah Bland <scb4ga@virginia.edu>
2018-01-03 15:36:29 -0500

    Drop down prints to DOM

--- homeless-shelters/index.js
+++ homeless-shelters/index.js
@@ -1,74 +1,69 @@
 
 // this is index.js
-
 // fetch API
-// fetch("https://data.cityofnewyork.us/Social-Services/Directory-Of-Homeless-Drop-In-Centers/bmxf-3rd4")
-// .then( r => r.json())
-// .then()
 
-//var submit = document.getElementById("submit")
-//console.log(submit)
+  fetch('https://data.cityofnewyork.us/resource/kjtk-8yxq.json')
+  .then( r => r.json() )
+  .then( options => showOptions(options) )
 
-//class App {
+
+// class App {
 
   const form = document.getElementsByTagName("form")[0]
-  const answer = document.getElementById("answer")
 
-  console.log(form)
   form.onsubmit = (e) => {
     e.preventDefault()
     //alert("You clicked on a title! Congratulations!")
     //console.log("submitted!")
   }
 
-  var input = document.getElementById("borough-options")
-  console.log(input)
-  input.onclick = () => {
-     showOptions(input)
+  var answer = document.getElementById("borough-options")
+  //console.log(answer)
+  answer.onclick = () => {
+     showOptions(answer)
   }
 
-function showOptions(s) {
-  console.log(s[s.selectedIndex].value )
+  function showOptions(s) {
+    document.getElementById("value").innerHTML = s[s.selectedIndex].value
 
-}
+  }
 
+  function handleSubmit(){
+     let resp = this.options[this.options.selectedIndex].value
+     switch(resp) {
+       case 'Manhattan':
 
-  // buildResponse(resp){
-  //   this.answer.innerHTML = ''
-  //   let p = document.createElement("p")
-  //   let text = document.createTextNode(resp)
-  //   p.appendchild(text)
-  //   this.answer.insertAdjacentElement('afterBegin', p)
-  // }
+        break;
+       case 'Bronx':
 
-  // handleSubmit(){
-  //    let resp = this.options[this.options.selectedIndex].value
-  //    switch(resp) {
-  //      case 'Manhattan':
-  //
-  //       break
-  //      case 'Bronx':
-  //
-  //       break
-  //      case 'Brooklyn':
-  //
-  //       break
-  //      case 'Queens':
-  //
-  //       break
-  //      case 'Staten Island':
-  //
-  //       break
-  //
-  //      default:
-  //          this.incorrectAnswer()
-  //        }
-  //  }
+        break;
+       case 'Brooklyn':
 
-   // blankAnswer() {
+        break;
+       case 'Queens':
+
+        break;
+       case 'Staten Island':
+
+        break;
+
+       default:
+           this.blankAnswer()
+         }
+   }
+
+  function buildResponse(resp){
+    this.answer.innerHTML = ''
+    let p = document.createElement("p")
+    let text = document.createTextNode(resp)
+    p.appendchild(text)
+    this.answer.insertAdjacentElement('afterBegin', p)
+  }
+   //
+   // function blankAnswer() {
    //   this.buildResponse("Please select an option")
    // }
-
+   //
 
 
 //}
