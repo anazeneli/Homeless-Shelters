b4e06a2bed53c464fb680afc9d1124ee6333d5fe
Ana Zeneli <anazeneli@Anas-MacBook-Pro.local>
2018-01-03 14:06:36 -0500

    Added drop down menu

--- homeless-shelters/index.js
+++ homeless-shelters/index.js
@@ -1,1 +1,34 @@
 // this is index.js
+
+// fetch API
+// fetch("https://data.cityofnewyork.us/Social-Services/Directory-Of-Homeless-Drop-In-Centers/bmxf-3rd4")
+// .then( r => r.json())
+// .then()
+
+
+
+
+
+handleSubmit(){
+   let resp = this.options[this.options.selectedIndex].value
+   switch(resp) {
+     case 'Manhattan':
+
+      break
+     case 'Bronx':
+
+      break
+     case 'Brooklyn':
+
+      break
+     case 'Queens':
+
+      break
+     case 'Staten Island':
+
+      break
+
+     default:
+         this.incorrectAnswer()
+       }
+ }
